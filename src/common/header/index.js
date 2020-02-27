41e7cf6080bf3daaf7a4a1607fde338ab336d093
qiuxan <qiuxan@gmail.com>
2020-02-27 22:48:58 +1100

    first commit

---
+++ src/common/header/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { HeaderWrapper, Logo, Nav } from "./style";
+class Header extends Component {
+  render() {
+    return (
+      <HeaderWrapper>
+        <Logo href="/" />
+        <Nav></Nav>
+      </HeaderWrapper>
+    );
+  }
+}
+export default Header;
