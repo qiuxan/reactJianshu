b4a3beb977494b9126d5a852d495c28fb1735dbe
qiuxan <qiuxan@gmail.com>
2020-03-08 13:13:22 +1100

    redux-immutable plug in

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -101,7 +101,8 @@ const Header = props => {
 
 const mapStateToProps = state => {
   return {
-    focused: state.header.get("focused")
+    focused: state.getIn(["header", "focused"])
+    // focused: state.get("header").get("focused")
   };
 };
 const mapDispatchToProps = dispatch => {
