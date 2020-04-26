2c18ac49708de239a432c52c0c5ad8b31849131b
qiuxan <qiuxan@gmail.com>
2020-04-26 22:43:24 +1000

    router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from "react";
 import { Provider } from "react-redux";
 import { GlobalStyle } from "./style";
 import { IconlStyle } from "./statics/iconfont/iconfont";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./common/header";
 import store from "./store";
 
 function App() {
   return (
     <Provider store={store}>
-      <GlobalStyle />
-      <IconlStyle />
-      <Header />
+      <div>
+        <GlobalStyle />
+        <IconlStyle />
+        <Header />
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact render={() => <div>home</div>}></Route>
+            <Route
+              path="/detail"
+              exact
+              render={() => <div>detail</div>}
+            ></Route>
+          </div>
+        </BrowserRouter>
+      </div>
     </Provider>
   );
 }
